f507a449a8fc8ff0d3ca9a2eb2579f6c35d9f735
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-01 11:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+<<<<<<< HEAD
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
@@ -20,4 +21,8 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase() === string)
   return "YES INDEED!"
   else return "I love you, too."
+=======
+function logShout(string) {
+  console.log(string.toUpperCase())
+>>>>>>> 852b986f44dac86dbad180a88d6c46aebf9dc6a3
 }
